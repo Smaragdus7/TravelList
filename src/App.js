2023e22d7fa8eb4994e75fca54dd65afb3c77722
Smaragdus7 <smar.leeft@gmail.com>
2024-06-24 17:04:32 -0600

    sorted

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { useState } from "react";
 
 export default function App() {
@@ -58,10 +59,22 @@ function Form({onAddItems}){
   </form>
 }
 function PackingList({items, onDeleteItem, onToggleItem}){
+  const [sortBy, setSortBy] = useState('input');
+  let sortedItems;
+  if(sortBy === 'items'){ sortedItems = items;}
+  if(sortBy === 'description'){ sortedItems = items.slice().sort((a,b) => a.description)}
+  if(sortBy === 'status'){ sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));}
   return <div >
     <ul className="list">
-      {items.map((item) => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
+      {sortedItems.map((item) => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
     </ul>
+    <div className="actions">
+      <select value={sortBy} onChange={e=>setSortBy(e.target.value)}>
+        <option value='input'>Sort by input order</option>
+        <option value='description'>Sort by description</option>
+        <option value='packed'>Sort by status</option>
+      </select>
+    </div>
   </div>
 }
 function Item({item, onDeleteItem, onToggleItem}){
